a07a96c7bf8b34adca09279a7a5ae4306117d892
jhorgan3672 <jhorgan3672@gmail.com>
2019-12-09 14:14:44 -0600

    Update 09DEC2019

--- data/allStops.js
+++ data/allStops.js
@@ -1,4 +1,4 @@
-{
+var allStops = {
   "type": "FeatureCollection",
   "features": [
     {
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const mymap = L.map('mapid', {zoomControl:true, maxZoom:20, minZoom:3}).setView([46.7324, -117.0002], 15);
+const mymap = L.map('mapid', {zoomControl:true, maxZoom:16, minZoom:3}).setView([46.7324, -117.0002], 15);
 	
 	const basemap = new L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
@@ -7,7 +7,10 @@ const mymap = L.map('mapid', {zoomControl:true, maxZoom:20, minZoom:3}).setView(
 			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		id: 'mapbox.dark'
 	}).addTo(mymap);
-        
+
+    
+
+    
 var greenRouteStyle1 = {
     color: 'Chartreuse',
     weight: 2  
@@ -40,7 +43,7 @@ const greenStopsStyle = {
         dashArray: '',
         lineCap: 'butt',
         lineJoin: 'miter',
-        weight: 1.0,
+        weight: 2.0,
         fill: true,
         fillOpacity: 1,
         fillColor: 'Green'
@@ -53,7 +56,7 @@ const greenStopsStyle = {
         dashArray: '',
         lineCap: 'butt',
         lineJoin: 'miter',
-        weight: 1.0,
+        weight: 2.0,
         fill: true,
         fillOpacity: 1,
         fillColor: 'Blue'
@@ -66,21 +69,53 @@ const greenStopsStyle = {
         dashArray: '',
         lineCap: 'butt',
         lineJoin: 'miter',
-        weight: 1.0,
+        weight: 2.0,
         fill: true,
         fillOpacity: 1,
-        fillColor: 'Orange'
+        fillColor: 'grey'
         };
 
 
+//function mapGreen(feature1, feature2) {
+//L.geoJSON(feature1, {style: greenRouteStyle1}).addTo(mymap);
+//L.geoJSON(feature2,{style: greenRouteStyle2}).addTo(mymap);
+//}        
+        
 
 
-L.geoJSON(greenRoute2, {style: greenRouteStyle2}).addTo(mymap);
-L.geoJSON(greenRoute1,{style: greenRouteStyle1}).addTo(mymap);
-L.geoJSON(blueRoute2, {style: blueRouteStyle2}).addTo(mymap);
-L.geoJSON(blueRoute1,{style: blueRouteStyle1}).addTo(mymap);
-L.geoJSON(sharedRoute, {style: shareRouteStyle}).addTo(mymap);
+const gl2 = L.geoJSON(greenRoute2, {style: greenRouteStyle2}).addTo(mymap);
+const gl1 = L.geoJSON(greenRoute1,{style: greenRouteStyle1}).addTo(mymap);
+const bl2 = L.geoJSON(blueRoute2, {style: blueRouteStyle2}).addTo(mymap);
+const bl1 = L.geoJSON(blueRoute1,{style: blueRouteStyle1}).addTo(mymap);
+//L.geoJSON(sharedRoute, {style: shareRouteStyle}).addTo(mymap);
 const green = L.geoJSON(greenStops, {pointToLayer: function (point, latlng){ return L.circleMarker(latlng, greenStopsStyle).bindPopup("Stop: " + point.properties.Stop)}}).addTo(mymap);
 const blue = L.geoJSON(blueStops, {pointToLayer: function (point, latlng){ return L.circleMarker(latlng, blueStopsStyle).bindPopup("Stop: " + point.properties.Stop)}}).addTo(mymap);
-const orange = L.geoJSON(sharedStops, {pointToLayer: function (point, latlng){ return L.circleMarker(latlng, sharedStopsStyle).bindPopup("Stop: " + point.properties.Stop)}}).addTo(mymap);
+const orange = L.geoJSON(allStops, {pointToLayer: function (point, latlng){ return L.circleMarker(latlng, sharedStopsStyle).bindPopup("Stop: " + point.properties.Stop)}}).addTo(mymap);
+
+
+
+
+const routesLayer = {
+		
+    
+    "Green Line 1": gl1,
+    "Green Line 2": gl2,
+    "Blue Line 1": bl1,
+    "Blue Line 2": bl2 
+};
+
+const stopsLayer = {
+		
+    "Green Stops": green,
+    "Blue Stops": blue,
+    "All Stops": orange
+    
+};
+
+
+
+
+
+L.control.layers(stopsLayer, null,{collapsed:false}).addTo(mymap);
+L.control.layers(routesLayer,null,{collapsed:false}).addTo(mymap);
 
